18e6858e9e43f80f24e006ec841d9f7875b2be50
berkaybasoz <berkay.basoz@softtech.com.tr>
2025-10-01 22:37:56 +0300

    git cmd

---
+++ cmd/git_cmd.sh
@@ -0,0 +1,10 @@
+ git checkout main
+
+# Main branch'i güncelle (opsiyonel ama önerilen)
+git pull origin main
+
+# test-screen branch'ini main'e merge et
+git merge test-screen
+
+# Merge'i remote repository'ye push et
+git push origin main
